dc3b8f244441d25917994d028a702fbdcb14c0c7
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-11-22 15:26:35 -0600

    inital commit, has a fillable text file and button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component{
+  state={
+    todoList:[
+      { 
+        id:"",
+        todo:"",
+      }
 
-export default App;
+    ]
+
+
+    
+  }
+  
+  render(){
+return (
+// alters the alignment of the div
+<div style ={{textAlign:"center"}}>
+  {/* creates a text field */}
+  <input type="text" name="todo"/>
+  {/* adds a button on the side */}
+  <button>Add</button>
+{/* removes bullet points from list */}
+  <ul style={{listStyle:"none"}}>
+
+  </ul>
+  {/* loops through the given todos and renders the values of the todo */}
+  lol @ {this.state.todoList.map(({id, todo})=>{
+  return <li key={id}>{todo}</li> 
+})} </div>
+  )
+
+  }
+  }
+
+  
+  //what actually shows up on the page
+
+
+
+// export default App;
