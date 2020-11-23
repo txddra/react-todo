865cbee6cd46caf103ee88f8066431e81a300582
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-11-23 08:46:43 -0600

    created a components folder

---
+++ src/components/TodoView.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TodoView = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+// TodoView.propTypes = {
+
+// }
+
+export default TodoView
