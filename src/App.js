e4a976daf23dc301b58d09b0c216517cf9d61cb7
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-11-30 10:45:04 -0600

    ,

--- src/App.js
+++ src/App.js
@@ -6,23 +6,33 @@ export class App extends Component {
   state ={
     isAuth : false
   }
+handleOnChange=(event)=>{
+
+}
+
+
+handleOnSubmit =(event)=>{
+
+}
+
+
   render() {
     const {isAuth} =this.state;
 
     let showComponent = isAuth ? (<Todo />) :(
 
-      <form>
+      <form onSubmit ={}>
       {" "}
-    <input type="text" placeholder="enter email"/> 
+    <input type="text" placeholder="enter email" name="email" onChange ={this.handleOnchange}/> 
     <br />{" "}
 
-    <input type = "text" placeholder="enter Password"/>
+    <input type = "text" placeholder="enter Password" onChange ={this.handleOnchange}/>
     <br />
     {" "}
     <button>Sign Up</button>
     </form>
       );
-    return <div style={{textAlign:"center"}}>
+    return <div style={{textAlign:"center", marginTop:"15%"}}>
         {showComponent}
       </div>
     
