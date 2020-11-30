a52c811a2dfb559378827b5f2d6a2e23c8b283b0
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-11-30 10:32:16 -0600

    added sign in page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react'
+import Todo from "./components/Todo/Todo"
 
 export class App extends Component {
+
+  state ={
+    isAuth : false
+  }
   render() {
-    return (
-      <div>
-        
+    const {isAuth} =this.state;
+
+    let showComponent = isAuth ? (<Todo />) :(
+
+      <form>
+      {" "}
+    <input type="text" placeholder="enter email"/> 
+    <br />{" "}
+
+    <input type = "text" placeholder="enter Password"/>
+    <br />
+    {" "}
+    <button>Sign Up</button>
+    </form>
+      );
+    return <div style={{textAlign:"center"}}>
+        {showComponent}
       </div>
-    )
+    
   }
 }
 
